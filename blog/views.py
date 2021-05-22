c6b80312e04ce51544931ed1d7f6e77b4c09047b
Timofey1565 <timot156585@gmail.com>
2021-05-22 10:57:48 +0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def home(request):
+    return render(request, 'blog/home.html')
--- mysite1/urls.py
+++ mysite1/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('post_list/', include('blog.urls')),
+    path('', blog.views.home, name='home'),
 ]
